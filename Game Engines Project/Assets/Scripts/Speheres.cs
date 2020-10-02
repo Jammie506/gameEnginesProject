a6d6745e00011acb7c5dc66eb64980a7315a8c2f
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2020-10-02 17:10:04 +0100

    Instantiating
    Set up an array for Spehers to be instatiated on runtime with varying sze, position and speeds - Jamie

---
+++ Game Engines Project/Assets/Scripts/Speheres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Speheres : MonoBehaviour
+{
+    public GameObject centre;
+    private float _size;
+
+    private void Start()
+    {
+        //sets the planets starting position
+        this.transform.position = new Vector3(Random.Range(5, 50), 0, Random.Range(5, 50));
+        //sets the size of each planet randomly
+        _size = Random.Range(0, 10);
+        this.gameObject.transform.localScale = new Vector3(_size,_size,_size);
+    }
+
+    void Update()
+    {
+        //makes the planets rotate around the centre of the scene (where the Controller gO is)
+        transform.RotateAround(centre.transform.position, Vector3.up, Random.Range(0, 50) * Time.deltaTime);
+    }
+}
